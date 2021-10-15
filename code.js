7c1ad201b7a95ad908be7a9de9d3ab88e3327a4e
147017 <87478712+147017@users.noreply.github.com>
2021-10-15 14:57:52 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,46 @@
+var pizza_order = [];
+
+function order1() {
+pizza_order.push(" Regular cheese");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order2() {
+pizza_order.push(" Supreme thin crust");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order3() {
+pizza_order.push("Cheese burst");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order4() {
+pizza_order.push(" Pepperoni party");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order5() {
+pizza_order.push(" Supreme tobasco sauced");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order6() {
+pizza_order.push(" Spicy pepper");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order7() {
+pizza_order.push(" Cinamon rolls");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order8() {
+pizza_order.push(" Thin crust");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order9() {
+pizza_order.push(" Super cheese burst");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order10() {
+pizza_order.push(" Spicy pepperoni party");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
+function order11() {
+pizza_order.push(" Supreme tomato sauced");
+document.getElementById("Order").innerHTML="Your order:-"+ pizza_order;
+}
---
+++ theme.js
@@ -0,0 +1,7 @@
+
+function dark_theme() {
+    document.body.style.backgroundColor = "black";
+}
+function light_theme() {
+    document.body.style.backgroundColor = "white";
+}
